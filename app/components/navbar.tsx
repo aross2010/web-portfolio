cf3a42e1100e4df07f4a235c44b9b2899810a9d6
Alex Ross <adross1027@gmail.com>
2024-08-28 22:33:59 -0700

    slider color change

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -43,7 +43,7 @@ export default function Navbar() {
           {label}
           {label === activeSection && (
             <motion.span
-              className="bg-sjsu-gold rounded-full absolute inset-0 -z-10 dark:bg-gray-700"
+              className="bg-sjsu-gold rounded-full absolute inset-0 -z-10"
               layoutId="activeSection"
               transition={{
                 type: 'spring',
